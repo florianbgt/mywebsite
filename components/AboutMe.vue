998a647b917a46f0baf43020aec53bfa512f2698
florianbgt <florianbgt@users.noreply.github.com>
2021-09-07 19:55:04 -0400

    change about me and skillset

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -1,18 +1,55 @@
 <template>
+  <div>
+    <h1 class="text-center">
+      About Me
+    </h1>
     <div>
-        <h1 class="text-center">
-            About Me
-        </h1>
-        <div>
-            <p><big>
-                Mechanical Engineer by training, and self taught web developer. I like being challenged and solve problems!
-            </big></p>
-            <p><big>
-                I am now focusing on developing web application using modern javascript web framework (Vue 3 and Nuxt) and Python based REST API (Django). I also use Docker extensively to ease deployment and maintenance
-            </big></p>
-            <p><big>
-                Feel free to email me if you want to chat! &#128512;
-            </big></p>
-        </div>
+      <p>
+        <big>
+          Mechanical Engineer by training, I use to work for the areospace and
+          the automotive industry developing new products for big industrial
+          customers.
+        </big>
+      </p>
+      <p>
+        <big>
+          Recently self taught web developer. I like to task automations and
+          developing powefull and responsive web applications to solve everyday
+          problems and boost productivity!
+        </big>
+      </p>
+      <p>
+        <big>
+          I love learning new technologies, being challenged and solve problems!
+        </big>
+      </p>
+      <p>
+        <big>
+          I am now focusing on developing web application using modern
+          javascript web framework (Vue 3 and Nuxt) and Python based REST API
+          (Django). I also use Docker extensively to ease deployment and code
+          maintenance.
+        </big>
+      </p>
+      <p>
+        <big>
+          I also have a blog where I write about the techs I use.
+          <strong
+            ><a href="https://blog.florianbgt.com/" target="_blank"
+              >Check it out!</a
+            ></strong
+          >
+        </big>
+      </p>
+      <p>
+        <big>
+          Feel free to
+          <strong
+            ><a href="mailto:florian.bigot321@gmail.com">email me</a></strong
+          >
+          if you want to chat! &#128512;
+        </big>
+      </p>
     </div>
+  </div>
 </template>
--- components/MySkillset.vue
+++ components/MySkillset.vue
@@ -2,12 +2,12 @@
     <div>
         <h1 class="text-center">My Skillset</h1>
         <b-row class="mt-5">
-            <b-col sm=6>
+            <b-col md=12 sm=6>
                 <SkillItem level="5" label="Vue.js">
                     <template>
                         <strong>My favorite Frontend Framework!</strong>
                         <br>
-                        Vue 2 | Vue 3 | Nuxt.js | Nuxt Content
+                        Vue 2 | Vue 3 | Nuxt.js
                     </template>
                 </SkillItem>
             </b-col>
@@ -17,6 +17,15 @@
                         <strong>My favorite Backend Framework!</strong>
                         <br>
                         Django | Django REST
+                    </template>
+                </SkillItem>
+            </b-col>
+            <b-col sm=6>
+                <SkillItem level=4 label="Docker">
+                    <template>
+                        <strong>Make deployment a breeze</strong>
+                        <br>
+                        Docker | Docker-compose | AWS ECS
                     </template>
                 </SkillItem>
             </b-col>
@@ -40,25 +49,27 @@
                 </SkillItem>
             </b-col>
             <b-col sm=6>
-                <SkillItem level=4 label="Docker">
-                    <template>
-                        <strong>Make deployment a breeze</strong>
-                    </template>
-                </SkillItem>
-            </b-col>
-            <b-col sm=6>
-                <SkillItem level="3" label="Version Control">
-                    <template>
-                        Git
-                    </template>
-                </SkillItem>
-            </b-col>
-            <b-col md=12 sm=6>
                 <SkillItem level="3" label="Databases">
                     <template>
                         <strong>SQL & noSQL</strong>
                         <br>
-                        Postgres | MongoDB | Firebase
+                        Postgres | MongoDB
+                    </template>
+                </SkillItem>
+            </b-col>
+            <b-col sm=6>
+                <SkillItem level="3" label="Cloud solutions">
+                    <template>
+                        <strong>AWS & Firebase</strong>
+                        <br>
+                        AWS RDS | AWS S3 | AWS ECS | Firebase
+                    </template>
+                </SkillItem>
+            </b-col>
+            <b-col md=12 sm=6>
+                <SkillItem level="3" label="Version Control">
+                    <template>
+                        Git
                     </template>
                 </SkillItem>
             </b-col>
